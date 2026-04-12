c5592a37a471c5e6f85dcb70988b11e5a4e09f55
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2026-04-12 18:46:36 +0530

    add FlatList header component

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,14 @@
+import CartButton from "@/components/CartButton";
 import { images, offers } from "@/constants";
 import React, { Fragment } from "react";
-import { FlatList, Image, Pressable, Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  Pressable,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
 
@@ -49,6 +57,24 @@ export default function Index() {
           );
         }}
         contentContainerClassName="pb-28 px-5"
+        ListHeaderComponent={() => (
+          <View className="flex-between flex-row w-full my-5 px-5">
+            <View className="flex-start">
+              <Text className="small-bold text-primary">DELIVER TO</Text>
+
+              <TouchableOpacity className="flex-center flex-row gap-x-1 mt-0.5">
+                <Text className="paragraph-bold text-dark-100">Mumbai</Text>
+                <Image
+                  source={images.arrowDown}
+                  className="size-3"
+                  resizeMode="contain"
+                />
+              </TouchableOpacity>
+            </View>
+
+            <CartButton />
+          </View>
+        )}
       />
     </SafeAreaView>
   );
