3e340ce6db74b1422bf1ab0deb0b2b5161aa8f29
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2026-04-12 18:19:14 +0530

    add cards in homepage

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,55 @@
-import { Text, View } from "react-native";
+import { images, offers } from "@/constants";
+import React, { Fragment } from "react";
+import { FlatList, Image, Pressable, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import "../global.css";
 
 export default function Index() {
   return (
-    <SafeAreaView>
-      <View className="flex justify-center items-center h-full">
-        <Text className="text-blue-500 text-4xl text-center font-quicksand-semibold">
-          Edit app/index.tsx to edit this screen.
-        </Text>
-      </View>
+    <SafeAreaView className="flex-1 bg-white">
+      <FlatList
+        data={offers}
+        renderItem={({ item, index }) => {
+          const isEven = index % 2 === 0;
+
+          return (
+            <View>
+              <Pressable
+                className={`offer-card ${isEven ? "flex-row-reverse" : "flex-row"}`}
+                style={{ backgroundColor: item.color }}
+                android_ripple={{ color: "#ffffff22" }}
+              >
+                {({ pressed }) => (
+                  <Fragment>
+                    <View className={"h-full w-1/2"}>
+                      <Image
+                        source={item.image}
+                        className="size-full"
+                        resizeMode="contain"
+                      />
+                    </View>
+
+                    <View
+                      className={`offer-card__info ${isEven ? "pl-10" : "pr-10"}`}
+                    >
+                      <Text className="h1-bold text-white leading-tight">
+                        {item.title}
+                      </Text>
+                      <Image
+                        source={images.arrowRight}
+                        className="size-10"
+                        tintColor="#fff"
+                        resizeMode="contain"
+                      />
+                    </View>
+                  </Fragment>
+                )}
+              </Pressable>
+            </View>
+          );
+        }}
+        contentContainerClassName="pb-28 px-5"
+      />
     </SafeAreaView>
   );
 }
