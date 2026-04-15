5d8a2f2021368b79b678347b65fb3ba215bb5cd1
Altamash-Khan-2812 <altamashkhn281@gmail.com>
2026-04-15 14:53:36 +0530

    Implement tab navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,102 @@
+import { Image, Text, View } from "react-native";
 import { useAuthStore } from "../../store/auth.store";
-import { Redirect, Slot } from "expo-router";
+import { Redirect, Tabs } from "expo-router";
+import { TabBarIconProps } from "@/type";
+import { images } from "@/constants";
 
 export default function TabLayout() {
   const { isAuthenticated } = useAuthStore();
 
   if (!isAuthenticated) return <Redirect href="/sign-in" />;
 
-  return <Slot />;
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          borderTopLeftRadius: 50,
+          borderTopRightRadius: 50,
+          borderBottomLeftRadius: 50,
+          borderBottomRightRadius: 50,
+          marginHorizontal: 20,
+          height: 80,
+          position: "absolute",
+          bottom: 40,
+          backgroundColor: "#fff",
+          shadowColor: "#1a1a1a",
+          shadowOffset: {
+            width: 0,
+            height: 2,
+          },
+          shadowOpacity: 0.1,
+          shadowRadius: 4,
+          elevation: 5,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon title="Home" focused={focused} icon={images.home} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="search"
+        options={{
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon title="Search" focused={focused} icon={images.search} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="cart"
+        options={{
+          title: "Cart",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon title="Cart" focused={focused} icon={images.bag} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon
+              title="Profile"
+              focused={focused}
+              icon={images.person}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
+
+const TabBarIcon = ({ title, focused, icon }: TabBarIconProps) => {
+  return (
+    <View className="tab-icon">
+      <Image
+        source={icon}
+        className="size-7"
+        resizeMode="contain"
+        tintColor={focused ? "#FE8C00" : "#5D5f6D"}
+      />
+      <Text
+        className={`text-sm font-bold ${focused ? "text-primary" : "text-gray-200"}`}
+      >
+        {title}
+      </Text>
+    </View>
+  );
+};
